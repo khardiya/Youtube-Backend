40e7a8c6b000dede4a758e9afde74405bc5f9c8d
Bablu Arya <khardiya8824063605@gmail.com>
2025-09-11 23:04:41 +0530

    all setup config

--- src/app.js
+++ src/app.js
@@ -4,14 +4,26 @@ import cors from "cors";
 
 const app = express();
 const allowedOrigins = process.env.CLIENT_URL || '*';
-app.use(express.json({limit:'20kb'}));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
-app.use(cookieParser());
+
 app.use(cors({
     origin:allowedOrigins,
     methods:["GET","POST","PUT","DELETE"],
     credentials:true,
 }));
+app.use(express.json({limit:'20kb'}));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(cookieParser());
+
+
+// user routes import
+import userRouter from "./routes/user.routes.js";
+
+// router delclaration
+
+app.use("/api/v1/user", userRouter);
+
+// example route
+// http://localhost:8000/api/v1/user/register
 
 export default app;
