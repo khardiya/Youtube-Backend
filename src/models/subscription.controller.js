96dca03ce8e55f8719269d4f67679593559d750e
Bablu Arya <khardiya8824063605@gmail.com>
2025-09-15 16:07:00 +0530

    Fix typo in subscription schema: corrected 'subscribar' to 'subscriber'

--- src/models/subscription.controller.js
+++ src/models/subscription.controller.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const subscriptionSchema = new mongoose.Schema({
-    subscribar:{
+    subscriber:{
         type:mongoose.Schema.Types.ObjectId,
         ref:"User",
     },
