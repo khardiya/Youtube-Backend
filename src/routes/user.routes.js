93ecf5a716323e42832c4cee4b26e787be172379
Bablu Arya <khardiya8824063605@gmail.com>
2025-09-15 17:01:25 +0530

    add  route handlers for change password, current user, update account, update avatar, and update cover image get channel watch-histroy

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,22 @@
 import { Router } from "express";
-import { logoutUser, refreshAccessToken, registerUser, userlogin } from "../controllers/user.controller.js";
+import {
+    changeCurrentPassword,
+    getCurrentUser,
+    getUserChannelProfile,
+    getWatchHistory,
+    logoutUser,
+    refreshAccessToken,
+    registerUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage,
+    userlogin,
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
+// register and login routes (public)
 router.route("/register").post(
     upload.fields([
         { name: "avatar", maxCount: 1 },
@@ -11,9 +24,21 @@ router.route("/register").post(
     ]),
     registerUser
 );
-
 router.route("/login").post(userlogin);
+//  Protected Routes
+//  logout, refresh token, change password, get current user, update account details, update avatar, update cover image, channel profile , watch history
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+router
+    .route("/update-avatar")
+    .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router
+    .route("/update-cover")
+    .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/history").get(verifyJWT, getWatchHistory);
 
 export default router;
