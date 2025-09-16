15b547d2401e51e4e54e3b57e821ffa7f417eb5f
Bablu Arya <khardiya8824063605@gmail.com>
2025-09-16 09:52:04 +0530

    Fix healthcheck route import and update response formatting

--- src/app.js
+++ src/app.js
@@ -20,11 +20,14 @@ app.use(cookieParser());
 //  routes import
 import userRouter from "./routes/user.routes.js";
 import tweetRouter from "./routes/tweet.routes.js";
+import healthRouter from "./routes/healthcheck.routes.js";
+
 
 // router delclaration
 
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/tweet", tweetRouter);
+app.use("/api/v1/healthcheack", healthRouter);
 
 // example route
 // http://localhost:8000/api/v1/user/register
