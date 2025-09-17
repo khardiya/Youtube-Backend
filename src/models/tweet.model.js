72a723513f55e668c33a31d3656b7377986cdbfb
Bablu Arya <khardiya8824063605@gmail.com>
2025-09-17 23:50:33 +0530

    add pre hook if this tweet delete so its comments and like also delete

--- src/models/tweet.model.js
+++ src/models/tweet.model.js
@@ -14,4 +14,14 @@ const tweetSchema = new mongoose.Schema(
     { timestamps: true }
 );
 
+tweetSchema.pre("deleteOne", { document: true, query: false }, async function (next) {
+    try {
+        const tweetId = this._id;
+        await mongoose.model("Comment").deleteMany({ tweet: tweetId });
+        await mongoose.model("Like").deleteMany({ tweet: tweetId });
+        next();
+    } catch (error) {
+        throw new Error(error);
+    }
+});
 export const Tweet = mongoose.model("Tweet", tweetSchema);
