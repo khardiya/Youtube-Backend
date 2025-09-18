dd11e246b27cbcff3da1ebb5e04c546b19ab9728
Bablu Arya <khardiya8824063605@gmail.com>
2025-09-19 01:00:49 +0530

    import all routes

--- src/app.js
+++ src/app.js
@@ -22,14 +22,25 @@ import userRouter from "./routes/user.routes.js";
 import tweetRouter from "./routes/tweet.routes.js";
 import healthRouter from "./routes/healthcheck.routes.js";
 import videoRouter from "./routes/video.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js"
+import commentRouter from "./routes/comment.routes.js"
+import likeRouter from "./routes/like.routes.js"
+import playlistRouter from "./routes/playlist.routes.js"
+import dashboardRouter from "./routes/dashboard.routes.js"
+
 
 
 // router delclaration
 
-app.use("/api/v1/user", userRouter);
-app.use("/api/v1/tweet", tweetRouter);
-app.use("/api/v1/healthcheack", healthRouter);
-app.use("/api/v1/video", videoRouter);
+app.use("/api/v1/healthcheck", healthRouter)
+app.use("/api/v1/users", userRouter)
+app.use("/api/v1/tweets", tweetRouter)
+app.use("/api/v1/subscriptions",subscriptionRouter )
+app.use("/api/v1/videos", videoRouter)
+app.use("/api/v1/comments", commentRouter)
+app.use("/api/v1/likes", likeRouter)
+app.use("/api/v1/playlist", playlistRouter)
+app.use("/api/v1/dashboard", dashboardRouter)
 
 
 
