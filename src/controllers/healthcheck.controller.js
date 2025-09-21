bc705223d7d4578646c91a1d4156dcdf1f76ab01
Bablu Arya <khardiya8824063605@gmail.com>
2025-09-21 22:40:16 +0530

    add false condition we not GET request come

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -4,6 +4,9 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 const healthcheck = asyncHandler(async (req, res) => {
     //TODO: build a healthcheck response that simply returns the OK status as json with a message
+    if (req.method !== "GET") {
+        throw new ApiError(405, "Method Not Allowed");
+    }
     res.status(200).json(
         new ApiResponse(200, "OK", "Service is up and running")
     );
