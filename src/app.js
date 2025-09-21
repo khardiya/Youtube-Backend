631f2e01f61a28676345be4629bdbbca2d71c952
Bablu Arya <khardiya8824063605@gmail.com>
2025-09-21 22:39:38 +0530

    fix in import statement and router declaration

--- src/app.js
+++ src/app.js
@@ -17,32 +17,27 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
-//  routes import
+//routes import
+import healthcheckRouter from "./routes/healthcheck.routes.js";
 import userRouter from "./routes/user.routes.js";
 import tweetRouter from "./routes/tweet.routes.js";
-import healthRouter from "./routes/healthcheck.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
 import videoRouter from "./routes/video.routes.js";
-import subscriptionRouter from "./routes/subscription.routes.js"
-import commentRouter from "./routes/comment.routes.js"
-import likeRouter from "./routes/like.routes.js"
-import playlistRouter from "./routes/playlist.routes.js"
-import dashboardRouter from "./routes/dashboard.routes.js"
+import commentRouter from "./routes/comment.routes.js";
+import likeRouter from "./routes/like.routes.js";
+import playlistRouter from "./routes/playlist.routes.js";
+import dashboardRouter from "./routes/dashboard.routes.js";
 
-
-
-// router delclaration
-
-app.use("/api/v1/healthcheck", healthRouter)
-app.use("/api/v1/users", userRouter)
-app.use("/api/v1/tweets", tweetRouter)
-app.use("/api/v1/subscriptions",subscriptionRouter )
-app.use("/api/v1/videos", videoRouter)
-app.use("/api/v1/comments", commentRouter)
-app.use("/api/v1/likes", likeRouter)
-app.use("/api/v1/playlist", playlistRouter)
-app.use("/api/v1/dashboard", dashboardRouter)
-
-
+//routes declaration
+app.use("/api/v1/healthcheck", healthcheckRouter);
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/tweet", tweetRouter);
+app.use("/api/v1/subscription", subscriptionRouter);
+app.use("/api/v1/video", videoRouter);
+app.use("/api/v1/comment", commentRouter);
+app.use("/api/v1/likes", likeRouter);
+app.use("/api/v1/playlist", playlistRouter);
+app.use("/api/v1/dashboard", dashboardRouter);
 
 // example route
 // http://localhost:8000/api/v1/user/register
